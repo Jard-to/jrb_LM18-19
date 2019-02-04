9bb9dbead4d9fb11fac76742e80f29951f4ef654
Alumno <jardto@hotmail.com>
2019-02-04 14:35:17 +0100

    x

---
+++ PHP/ejercicio03/crear tabla for.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+>
+</head>
+<body>
+    <?php
+  <table>
+  $filas = 3
+  $columnas = 4  
+  echo "  <table style='border: 2px solid black'>"
+  for ($i=0; $i < $filas; $i++){
+      echo "<tr style='border: 2px solid black'>";
+  }
+  for ($j=0; $j < $columnas ; $j++){
+      echo "<td style='border: 2px solid black'>; 
+  } 
+  </table>
+</body>
+</html>
---
+++ PHP/ejercicio03/estructuras.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Estructuras</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   </head>
+<body>
+    <?php
+    for ($i=6);$i >= 0;$i--) {
+        if $i % 2 == 0)
+            print "<p>$i</p>";
+
+    }
+    
+</body>
+</html>
