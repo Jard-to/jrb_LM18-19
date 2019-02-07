764ae964217d14beb992278ebfc0469a2040a9a8
Alumno <jardto@hotmail.com>
2019-02-07 08:47:46 +0100

    x

---
+++ PHP/ejercicio04/tablamultiplicar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>tablas de multiplicar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+</head>
+<body>
+<?php
+$i=1
+for ($i=1;i<=10;$i++) {
+   for($j=0;)
+}
+
+    
+</body>
+</html>
