91f1884f718ff03e7dfe3a38dcca868f9d3f5bb3
Alumno <jardto@hotmail.com>
2019-02-25 13:20:51 +0100

    x

--- PHP/ejercicio11/MasaCorporal.php
+++ PHP/ejercicio11/MasaCorporal.php
@@ -4,26 +4,26 @@ $altura = trim(htmlspecialchars($_REQUEST["altura"], ENT_QUOTES, "UTF-8"));
 $pesok = false;
 $alturak = false;
 define('UNMETRO', 100);
-if (empty($pesok)) {
+if (empty($peso)) {
     print "<p>Campo Peso vacío</p>";
-} else if (!filter_var($pies, FILTER_VALIDATE_INT)) {
+} else if (!filter_var($peso, FILTER_VALIDATE_INT)) {
     print "<p>Campo Peso No es un Número Entero</p>";
-} else if ($pies < 0) {
+} else if ($peso < 0) {
     print "<p>Campo Peso debe ser >= 0</p>";
 } else {
     $pesok = true;
 }
 if (empty($altura)) {
     print "<p>Campo altura vacío</p>";
-} else if (!filter_var($pulgadas, FILTER_VALIDATE_FLOAT)) {
+} else if (!filter_var($altura, FILTER_VALIDATE_FLOAT)) {
     print "<p>Campo altura No es un Número Decimal</p>";
-} else if ($pulgadas < 0) {
+} else if ($altura < 0) {
     print "<p>Campo altura debe ser >= 0</p>";
 } else {
-    $esok = true;
+    $pesok = true;
 }
 if ($pesok && $alturak) {
-    $masa = ($peso / ( $altura * $altura ) * UNAPULGADA;
-    print "<p>$pies pies y $pulgadas pulgadas son $cm centímetros</p>";
+    $masa = ($peso / ( $altura * $altura ) * UNMETRO;
+    print "<p>$peso en gramos y $altura  son $cm centímetros</p>";
 }
 ?>
