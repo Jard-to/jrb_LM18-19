f731cc2488672344e1e2a218fbcfea674a30166f
Alumno <jardto@hotmail.com>
2019-03-07 09:03:01 +0100

    actualizacion visual

--- PHP/ejercicio04/tablamultiplicar.php
+++ PHP/ejercicio04/tablamultiplicar.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>tablas de multiplicar</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-</head>
-<body>
 <?php
-$num= (1,10)
-for ($i=1;i<=100;$i++) {
+$numero = trim(htmlspecialchars($_REQUEST["numTabla"], ENT_QUOTES, "UTF-8"));
+for ($i=1; $i <= 10; $i++) { 
+    print "$numero x $i = " . $numero*$i . "<br/>";
 }
-$resultado = $num * $i
-echo "$num x $resultado";
 ?>
-    
-</body>
-</html>
--- PHP/ejercicio15/tercera.php
+++ PHP/ejercicio15/tercera.php
@@ -3,16 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Cookies</title>
+    <title>Sesiones</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
     <?php
-    setcookie("nombreUsu", "Javier", 0);
+        session_start();
+        print "Hola " . $_SESSION['nombreWeb'];
     ?>
-    <p>
-        <a href="primera.php">Ir a Primera</a>
-    </p>
-
 </body>
 </html>
