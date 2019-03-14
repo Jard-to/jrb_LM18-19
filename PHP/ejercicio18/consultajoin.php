b69b717e303bbfb84d2b21c61c25240801897c94
Alumno <jardto@hotmail.com>
2019-03-14 09:02:32 +0100

    x

--- PHP/ejercicio18/consultajoin.php
+++ PHP/ejercicio18/consultajoin.php
@@ -3,27 +3,33 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>consultajoin</title>
+    <title>Consulta Join</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <?php
-         $conexion = mysqli_connect("localhost", "root", "", "cursophp") or die("Problemas de conexión");
-        $registros = mysqli_query($conexion, "SELECT idAlumno, nombre, mail, codigocurso FROM alumnos 
-        as alu inner join cursos as cur on alu.codigocurso = cur.idCurso") 
-        or die("Problemas en la consulta:".mysqli_error($conexion));
-        echo "<table class='table table-striped'>";
-        echo "<tr><th> Código</th><th>Nombre</th><th>Email</th><th>Curso</th></tr>";
-        
-        while ($reg=mysqli_fetch_array($registros)) {
-            echo "<tr>";
-            echo "<td>" . $reg['idAlumno'] . "</td>";
-            echo "<td>" . $reg['nombre'] . "</td>";
-            echo "<td>" . $reg['mail'] . "</td>";
-            echo "<td>" . $reg['Curso'] . "</td>";
-            echo "</tr>";
-        }
-        mysqli_close($conexion);
+    <div class="container">
+        <?php
+            $conexion = mysqli_connect("localhost", "root", "", "cursophp") 
+            or die("Problemas con la conexión");
+            $registros = mysqli_query($conexion, 
+            "SELECT alu.idAlumno, alu.nombre, alu.mail, cur.nombreCurso 
+            from alumnos as alu inner join cursos as cur on alu.codigocurso = cur.idCurso") 
+            or die("Problemas en la consulta:".mysqli_error($conexion));
+            
+            echo "<table class='table table-striped'>";
+            echo "<tr><th>Código</th><th>Nombre</th><th>Email</th><th>Curso</th></tr>";
+                while ($reg = mysqli_fetch_array($registros)) {
+                    echo "<tr>";
+                        echo "<td>" . $reg['idAlumno'] . "</td>";
+                        echo "<td>" . $reg['nombre'] . "</td>";
+                        echo "<td>" . $reg['mail'] . "</td>";
+                        echo "<td>" . $reg['nombreCurso'] . "</td>";
+                    echo "</tr>";
+                }
+            echo "</table>";
+            mysqli_close($conexion);
     ?>
 </body>
 </html
